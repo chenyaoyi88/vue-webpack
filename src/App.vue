ee9bd2020c256b1e38968a8c5f1f58dce919cc88
chenyaoyi88 <152296058@qq.com>
2018-03-26 00:25:50 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'app'
+})
+</script>
+
+<style lang="scss">
+    @import './app.scss'
+</style>
